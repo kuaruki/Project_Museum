fdd088c6d4d4c40940b4a89b9b04e06ac430a6cd
PPalhinha <pacjtp@gmail.com>
2023-04-17 00:10:37 +0100

    Jigsaw Puzzle (Almost complete)
    missing snap and drop
    Can go from game to puzzle
    Cant get back to og pos

---
+++ Project Museum/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject SelectedPiece;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.transform.CompareTag("Puzzle")){
+                SelectedPiece = hit.transform.gameObject;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            SelectedPiece = null;
+        }
+        if(SelectedPiece != null)
+        {
+            Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3(MousePoint.x, MousePoint.y, 0);
+
+        }
+    }
+}
---
+++ Project Museum/Assets/Scripts/Open_Jigsaw.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Open_Jigsaw : MonoBehaviour, IInteractable
+{
+    public void Interact() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("Jigsaw2D");
+
+    }
+}
