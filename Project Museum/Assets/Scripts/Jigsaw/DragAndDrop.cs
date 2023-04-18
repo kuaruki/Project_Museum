5c0e7c2531bea0b4d66e955dd1d85c2947a009e0
PPalhinha <pacjtp@gmail.com>
2023-04-18 10:55:57 +0100

    JIGSAW IS WORKING

--- Project Museum/Assets/Scripts/Jigsaw/DragAndDrop.cs
+++ Project Museum/Assets/Scripts/Jigsaw/DragAndDrop.cs
@@ -4,18 +4,19 @@ using Unity.VisualScripting;
 using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
 
 public class DragAndDrop : MonoBehaviour
 {
     public GameObject SelectedPiece;
+    int OIL = 1;
 
     void Start()
     {
         
     }
 
-    void Update()
-    {
+    void Update(){
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -23,21 +24,27 @@ public class DragAndDrop : MonoBehaviour
             SceneManager.LoadScene("SampleScene");
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //Puzzle piece selected
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             
-            if (hit.transform.CompareTag("Puzzle")){
-                if (!hit.transform.GetComponent<PiecesScript>().inRightPosition) {  //this if makes it so that the pieces stay in place after being put in the right place
+            if (hit.transform.CompareTag("Puzzle"))
+            {
+                if (!hit.transform.GetComponent<PiecesScript>().inRightPosition) //this if makes it so that the pieces stay in place after being put in the right place
+                {  
                     SelectedPiece = hit.transform.gameObject;
                     SelectedPiece.GetComponent<PiecesScript>().selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL; // makes a piece in front of all the others
+                    OIL++;
                 }
             }
         }
         if (Input.GetMouseButtonUp(0))
         {
-            SelectedPiece.GetComponent<PiecesScript>().selected = false;
-            SelectedPiece = null;
+            if(SelectedPiece != null) {
+                SelectedPiece.GetComponent<PiecesScript>().selected = false;
+                SelectedPiece = null;
+            }
         }
         if(SelectedPiece != null)
         {
--- Project Museum/Assets/Scripts/Jigsaw/PiecesScript.cs
+++ Project Museum/Assets/Scripts/Jigsaw/PiecesScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering;
 public class PiecesScript : MonoBehaviour
 {
     Vector3 RightPosition;
@@ -10,18 +10,22 @@ public class PiecesScript : MonoBehaviour
     void Start()
     {
         RightPosition = transform.position;
+        Debug.Log("Right Position" + RightPosition);
         transform.position = new Vector3 (Random.Range(105f, 113f), Random.Range(-57f,-50.44f), RightPosition.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, RightPosition) < 1f)//Returns the distance between to positions (useful for the wand puzzle)
+        if(Vector3.Distance(transform.position, RightPosition) < 1f) //"Distance" returns the distance between to positions (useful for the wand puzzle)
         {
             if (!selected)
             {
-                transform.position = RightPosition;
-                inRightPosition = true;
+                if(inRightPosition == false) {
+                    transform.position = RightPosition;
+                    inRightPosition = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                }
             }
         } 
     }
